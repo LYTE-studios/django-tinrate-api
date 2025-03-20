17a7528466d80946143daadf2a4a2e5ad7f63a6e
manong02 <manon.golos@gmail.com>
2025-03-20 12:13:20 +0100

    added util to calculate the average rating from reviews

---
+++ src/users/utils/user_profile_utils.py
@@ -0,0 +1,9 @@
+# user_profile_utils.py
+
+def calculate_average_rating(reviews):
+    """
+    Calculate the average rating from a queryset of reviews.
+    Excludes reviews with null ratings.
+    """
+    reviews = reviews.exclude(rating__isnull=True)
+    return round(sum(review.rating for review in reviews) / reviews.count(), 2) if reviews else 0
