4438f4113e00215844c8fb7fd2ce060cf34f1735
manong02 <manon.golos@gmail.com>
2025-03-20 17:47:53 +0100

    added settings models and serializers

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,6 +1,21 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from users.views.user_views import UserView
+from users.views.profile_views import (
+    UserProfileViewSet,
+    ExperienceViewSet,
+    CareerViewSet,
+    EducationViewSet,
+    ReviewViewSet,
+)
+
+router = DefaultRouter()
+router.register(r'user_profile', UserProfileViewSet, basename='user_profile'),
+router.register(r'user_experiences', ExperienceViewSet, basename='user_experiences'),
+router.register(r'user_careers', CareerViewSet, basename='user_careers')
+router.register(r'user_educations', EducationViewSet, basename='user_educations')
+router.register(r'user_reviews', ReviewViewSet, basename='user_reviews')
 
 urlpatterns = [
     path('user_view/', UserView.as_view(), name='user_view'),
-]
+] + router.urls
